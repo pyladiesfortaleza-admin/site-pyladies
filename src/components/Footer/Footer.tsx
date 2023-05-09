508edef4b17bc1d05a1bbdbb8509d3f8ba82cce2
DeboraMuro <debora.flaviabm@hotmail.com>
2023-05-09 18:42:21 -0300

    estrutura incial do footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,30 @@
+import { Container, Grid } from "@mui/material";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Footer = () => {
   return (
     <>
-      <p>Footer</p>
+    <Container>
+      <Grid container>
+        <p>Redes sociais</p>
+        <Grid item>
+          {/* <FontAwesomeIcon icon="fa-brands fa-github" /> */}
+        </Grid>
+        <Grid item>
+
+        </Grid>
+        <Grid item>
+
+        </Grid>
+        <Grid item>
+
+        </Grid>
+      </Grid>
+
+      <Grid container>
+        <p>Entre em contato</p>
+      </Grid>
+    </Container>
     </>
   );
 };
