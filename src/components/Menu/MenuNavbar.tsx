8bfc11fff4224cf97b45b8993c7fb88550d64c8e
YgillahLymmah <limaygila@gmail.com>
2023-05-29 22:18:26 -0300

    nav tag addition

--- src/components/Menu/MenuNavbar.tsx
+++ src/components/Menu/MenuNavbar.tsx
@@ -3,20 +3,22 @@ import "./Menu.css";
 
 function MenuNavbar() {
   return (
-    <ul className="list">
-      <li className="item">
-        <Link to="/">Sobre</Link>
-      </li>
-      <li className="item">
-        <Link to="/participe">Participe da comunidade</Link>
-      </li>
-      <li className="item">
-        <Link to="/parceiros">Parceiros</Link>
-      </li>
-      <li className="item">
-        <Link to="/contatos">Contatos</Link>
-      </li>
-    </ul>
+    <nav className="top-menu">
+      <ul className="list">
+        <li className="item">
+          <Link to="/">Sobre</Link>
+        </li>
+        <li className="item">
+          <Link to="/participe">Participe da comunidade</Link>
+        </li>
+        <li className="item">
+          <Link to="/parceiros">Parceiros</Link>
+        </li>
+        <li className="item">
+          <Link to="/contatos">Contatos</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
