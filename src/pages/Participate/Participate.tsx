2fad6d0aef408e7354598be842c1283cf8934d21
YgillahLymmah <limaygila@gmail.com>
2023-05-29 21:35:08 -0300

    New pages

---
+++ src/pages/Participate/Participate.tsx
@@ -0,0 +1,10 @@
+function Participate() {
+    return (
+      <div>
+        <h1>Participe da comunidade</h1>
+        <p>Conteúdo da página</p>
+      </div>
+    );
+  }
+  export default Participate;
+  
